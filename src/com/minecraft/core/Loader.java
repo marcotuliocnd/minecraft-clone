069aa09b7523158950d1d8b244ffb2bdf60ba8b1
getuliobr <getulioregis@alunos.utfpr.edu.br>
2020-10-01 13:20:29 -0300

    more efficient way to render stuff

--- src/com/minecraft/core/Loader.java
+++ src/com/minecraft/core/Loader.java
@@ -1,6 +1,7 @@
 package com.minecraft.core;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,13 +16,14 @@ public class Loader {
 	private List<Integer> vaos = new ArrayList<Integer>();
 	private List<Integer> vbos = new ArrayList<Integer>();
 
-	public RawModel loadToVao(float[] positions) {
+	public RawModel loadToVao(float[] positions, int[] indices) {
 		int vaoId = createVAO();
 		
-		storeDataInAttributeList(0, positions);
-		unbindVAO();
+		this.bindIndicesBuffer(indices);
+		this.storeDataInAttributeList(0, positions);
+		this.unbindVAO();
 		
-		return new RawModel(vaoId, positions.length / 3);
+		return new RawModel(vaoId, indices.length);
 	}
 	
 	private int createVAO() {
@@ -53,6 +55,21 @@ public class Loader {
 	
 	private void unbindVAO() {
 		GL30.glBindVertexArray(0);
+	}
+	
+	private void bindIndicesBuffer(int[] indices) {
+		int vboID = GL15.glGenBuffers();
+		vbos.add(vboID);
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboID);
+		IntBuffer buffer = this.storeDataInIntBuffer(indices);
+		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+	}
+	
+	private IntBuffer storeDataInIntBuffer(int[] data) {
+		IntBuffer buffer = BufferUtils.createIntBuffer(data.length);
+		buffer.put(data);
+		buffer.flip();
+		return buffer;
 	}
 	
 	private FloatBuffer storeDataInFloatBuffer(float[] data) {
